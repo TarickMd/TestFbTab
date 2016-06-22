18cec2d147d3f36bc32bd0a85ac3ccffaed2995d
TarickMd <medonosec@gmail.com>
2016-06-22 20:07:29 +0300

    bug fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var app = express();
 
@@ -11,6 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
+//DEFINE ALL ROUTES
+app.post('/*', function(request, response) {
+    response.redirect('/');
+});
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
